b50f20955d58963b203b8a6a666923141a77ebce
Serge Pilipchuk <112853396+sergepilipchuk@users.noreply.github.com>
2024-05-28 11:44:34 +0400

    Add code changes after review

--- CS/MauiReportingApp/MainPage.xaml.cs
+++ CS/MauiReportingApp/MainPage.xaml.cs
@@ -9,18 +9,23 @@ namespace MauiReportingApp;
 public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
-
-        XtraReport report = CreateTableReportInWizard();
-
-        string resultFile = Path.Combine(FileSystem.Current.AppDataDirectory, report.Name + ".pdf");
-        report.ExportToPdf(resultFile);
-
-        pdfViewer.DocumentSource = PdfDocumentSource.FromFile(resultFile);
     }
 
-    public XtraReport CreateTableReportInWizard() {
-        XtraReportInstance report = new XtraReportInstance() { Name = "Sample" };
-        report.CreateDocument();
+    public async Task<XtraReport> CreateTableReportAsync() {
+        XtraReportInstance report = null;
+        await Task.Run(() =>
+        {
+            report = new XtraReportInstance() { Name = "Sample" };
+            report.CreateDocument();
+        });
         return report;
+    }
+
+    private async void ContentPage_Loaded(object sender, EventArgs e) {
+        XtraReport report = await CreateTableReportAsync();
+        string resultFile = Path.Combine(FileSystem.Current.AppDataDirectory, report.Name + ".pdf");
+        report.ExportToPdf(resultFile);
+        pdfViewer.DocumentSource = PdfDocumentSource.FromFile(resultFile);
+        loadingShimmer.IsLoading = false;
     }
 }
